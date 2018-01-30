7254fbfdd73b8204bcdd59ec6d48857f22e3d98f
Milos Knezevic <ushtipak@gmail.com>
2018-01-30 21:57:08 +0100

    Update RNN size | batch | layers :)

--- gimme-an-occupation.sh
+++ gimme-an-occupation.sh
@@ -10,7 +10,7 @@ curl -s https://raw.githubusercontent.com/ushtipak/rnn-playground/master/fetch-o
 
 echo -n "> train a model ... "
 cd /opt/rnn-karpathy
-th train.lua -data_dir /opt/rnn-karpathy/data -rnn_size 64 -batch_size 20 -num_layers 2 -dropout 0.4 -gpuid -1 >/dev/null 2>&1 && echo done
+th train.lua -data_dir /opt/rnn-karpathy/data -rnn_size 128 -batch_size 2 -num_layers 3 -dropout 0.6 -gpuid -1 >/dev/null 2>&1 && echo done
 
 echo "> generate occupations ..."
 sudo th sample.lua cv/$(ls cv/ -1t | head -1) -gpuid -1
