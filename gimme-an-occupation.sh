3cfebde56cea72269a472e48110bd164ad35aa44
Milos Knezevic <ushtipak@gmail.com>
2018-01-30 15:14:45 +0100

    Add model train :)

--- gimme-an-occupation.sh
+++ gimme-an-occupation.sh
@@ -8,3 +8,6 @@ echo -n "> fetch occupations from wikipedia ... "
 amazon-linux-extras install python3
 curl -s https://raw.githubusercontent.com/ushtipak/rnn-playground/master/fetch-occupations.py | python3 > /opt/rnn-karpathy/data/input.txt && echo done
 
+echo -n "> train a model ... "
+th train.lua -data_dir /opt/rnn-karpathy/data -rnn_size 64 -batch_size 20 -num_layers 2 -dropout 0.4 -gpuid -1 >/dev/null 2>&1 && echo done
+
